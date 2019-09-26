035f745a834fdf2090bd45697162b134012a43f4
johannescalvin <yangchengzhu2011@163.com>
2019-09-26 17:11:34 +0800

    先使用单一模块完成开发

--- blog-system-elasticsearch/src/main/java/tech/freecode/blogsystem/Main.java
+++
@@ -1,9 +0,0 @@
-//package tech.freecode.blogsystem;
-//
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        ElasticsearchRepository
-//    }
-////}
