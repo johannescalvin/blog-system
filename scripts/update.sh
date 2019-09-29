4ae071b7f01b405c632752482f39551c203388fe
johannescalvin <yangchengzhu2011@163.com>
2019-09-29 11:45:14 +0800

    创建自动部署相关的脚本文件

---
+++ scripts/update.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+echo '更新博客脚本开始执行'
+
+base_blog_url='http://localhost:8080'
+# 最近一个pull操作发生的时间
+last_pull_timestamp=`git --no-pager reflog --format="%C(auto)%H#%cD#%gs" | awk -F '#' '{if(match($3,/pull/)) print $2}' | head -1`
+
+# --no-pager 表示不使用分页程序
+# --name-status 显示每一次提交对文件的修改：A，新增；M,修改；D，删除；R100，重命名
+# --since 只显示在此时间之后的日志
+# tac 按行倒着输出, 这样就可以按照时间顺序对其重建
+
+# 修改
+modified_blogs=`git --no-pager log --pretty=oneline --name-status --since="$last_pull_timestamp" | grep '^M' | awk '{print $2}' | grep '.md$' | tac`
+for blog in $modified_blogs ; do
+    url=$base_blog_url'/'$blog
+    echo '尝试索引并更新 '$url
+    curl -X PUT $url
+done
+
+# 新增
+added_blogs=`git --no-pager log --pretty=oneline --name-status --since="$last_pull_timestamp" | grep '^A' | awk '{print $2}' | grep '.md$' | tac`
+for blog in $added_blogs ; do
+    url=$base_blog_url'/'$blog
+    echo '尝试添加 '$url
+    curl -X POST $url
+done
+
+# 删除
+added_blogs=`git --no-pager log --pretty=oneline --name-status --since="$last_pull_timestamp" | grep '^D' | awk '{print $2}' | grep '.md$' | tac`
+for blog in $added_blogs ; do
+    url=$base_blog_url'/'$blog
+    echo '尝试删除 '$url
+    curl -X DELETE $url
+done
+
+# 重命名
+renamed_blogs=`git --no-pager log --pretty=oneline --name-status --since="$last_pull_timestamp" | grep '^R' | awk '{print $2,$3}' | grep '.md$' | tac`
+for blog in $added_blogs ; do
+    delete_blog=`echo $blog | awk '{print $1}'`
+    add_blog=`echo $blog | awk '{print $2}'`
+    url=$base_blog_url'/'$delete_blog
+    echo '尝试删除 '$url
+    curl -X DELETE $url
+
+    url=$base_blog_url'/'$add_blog
+    echo '尝试添加 '$url
+    curl -X POST $url
+    
+done
+
